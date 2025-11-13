ca677780d766ddb8b34b87f141078a77fbb180de
gmedia <gmedia@flowyk.gmedia.id>
2025-11-13 14:30:56 +0700

    update setup

--- setup.sh
+++ setup.sh
@@ -9,3 +9,6 @@ sudo chown -R 1000:1000 /data/elasticsearch
 sudo chown -R 1000:1000 /data/kibana
 sudo chown -R 1000:1000 /data/elk-certs
 sudo chown -R 1000:1000 /etc/elastiflow
+
+echo "vm.max_map_count=262144" | sudo tee /etc/sysctl.d/99-elasticsearch.conf
+sudo sysctl --system
